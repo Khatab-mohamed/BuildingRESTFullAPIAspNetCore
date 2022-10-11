3d96d3c0cc7ffc67d10cfac7c1d69db6ce1c0438
Dev-khatab <khatap1@hotmail.com>
2022-10-11 04:59:33 -0700

    Getting Autors

---
+++ src/Library.API/Controllers/AurhorsController.cs
@@ -0,0 +1,19 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace Library.API.Controllers
+{
+    public class AurhorsController: Controller
+    {
+        private ILibraryRepository _libraryRepository ; 
+        public AurhorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
