e9ce0c319421ba64d816ee8a9f5ea83858d82abf
Dev-khatab <khatap1@hotmail.com>
2022-10-17 03:58:08 -0700

    Adding Xml Output Formatter

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -10,6 +10,7 @@ using Library.API.DTOs;
 using Library.API.Helpers;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace Library.API
 {
@@ -26,7 +27,11 @@ namespace Library.API
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+                setupAction.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
 
             // register the DbContext on the container, getting the connection string from
             // appSettings (note: use this during development; in a production environment,
@@ -64,6 +69,7 @@ namespace Library.API
                  .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
                   src.DateOfBirth.GetCurrentAge()));
                 cfg.CreateMap<Book, BookDto>();
+
                 });
 
             libraryContext.EnsureSeedDataForContext();
